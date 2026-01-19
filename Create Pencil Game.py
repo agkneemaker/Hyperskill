60336779989d4bc650bf407d094e8951b2c4871d
Agne <taylordwone@googlemail.com>
2026-01-19 13:57:41 +0800

    Update Create Pencil Game.py

--- Create Pencil Game.py
+++ Create Pencil Game.py
@@ -1,5 +1,6 @@
 pencils = 0
 
+# how many pencils
 while True:
     try:
         pencils = int(input("How many pencils?: "))
@@ -12,6 +13,7 @@ while True:
     except ValueError:
         pencils = print("The number of pencils must be numeric")
 
+# player names
 pencils_num = pencils * "|"
 name1, name2 = input("Please enter Player 1's name: "), input("Please enter Player 2's name: ")
 pick_user = input(f"Who will be the first player ({name1}, {name2}): ")
@@ -19,6 +21,7 @@ pick_user = input(f"Who will be the first player ({name1}, {name2}): ")
 while pick_user != name1 and pick_user != name2:
     pick_user = input(f"Choose between {name1} and {name2}: ")
 
+# game itself
 while pencils > 0:
     counter = 0
     if pick_user == name1:
