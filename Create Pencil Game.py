4ada6f22d6c83c3a04c6d2ddf4a5c5c7c4054889
Agne <taylordwone@googlemail.com>
2026-01-19 13:56:29 +0800

    Update Create Pencil Game.py

--- Create Pencil Game.py
+++ Create Pencil Game.py
@@ -1,16 +1,19 @@
 pencils = 0
-while pencils <= 0:
+
+while True:
     try:
         pencils = int(input("How many pencils?: "))
         if pencils < 0:
             pencils = int(input("The number of pencils must be numeric: "))
+        elif pencils == 0:
+            pencils = print("The number of pencils should be positive")
+        else:
+            break
     except ValueError:
-        pencils = int(input(("The number of pencils must be numeric: ")))
-        if pencils == 0:
-            pencils = int(input("The number of pencils should be positive: "))
+        pencils = print("The number of pencils must be numeric")
 
 pencils_num = pencils * "|"
-name1, name2 = str(input("Player 1's Name: ")), str(input("Player 2's Name: "))
+name1, name2 = input("Please enter Player 1's name: "), input("Please enter Player 2's name: ")
 pick_user = input(f"Who will be the first player ({name1}, {name2}): ")
 
 while pick_user != name1 and pick_user != name2:
@@ -22,7 +25,10 @@ while pencils > 0:
         print("|" * pencils)
         counter = int(input(f"{name1}'s turn: "))
         while counter not in range(1,4):
-            counter = int(input("Possible values: '1', '2' or '3': "))
+            try:
+                counter = int(input("Possible values: '1', '2' or '3': "))
+            except ValueError:
+                print("Please enter a numerical value between 1-3.")
         while counter > pencils:
             counter = int(input("Too many pencils were taken: "))
         pencils -= counter
@@ -32,7 +38,10 @@ while pencils > 0:
         print("|" * pencils)
         counter = int(input(f"{name2}'s turn: "))
         while counter not in range(1,4):
-            counter = int(input("Possible values: '1', '2' or '3': "))
+            try:
+                counter = int(input("Possible values: '1', '2' or '3': "))
+            except ValueError:
+                print("Please enter a numerical value between 1-3.")
         while counter > pencils:
             counter = int(input("Too many pencils were taken: "))
         pencils -= counter
